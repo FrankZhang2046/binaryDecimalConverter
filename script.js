b2cdca9ad26f5615f5a740866bf502b05ee46623
FrankZhang2046 <frankbusinessmail@gmail.com>
2020-10-19 18:59:32 -0400

    decimalToBinary finished

--- script.js
+++ script.js
@@ -1,47 +1,42 @@
 function decimalToBinary (decimalNumber, bit = 0, convertedBinary = null) {
-    if (decimalNumber < 2 || decimalNumber == NaN) {
-        console.log('decimalNumber is ', decimalNumber);
-        return;
+    if (decimalNumber === 1 ) {
+        if (convertedBinary === null) {return console.log(decimalNumber)}
+        else return console.log(`${convertedBinary}${'0'.repeat(bit-1)}1`)
     };
 
     let highestBit;
 
     if (bit === 0) {
-        console.log('checking highest bit');
+        if (decimalNumber === 0 && convertedBinary !== null) {
+            return console.log(convertedBinary);
+        }
         for (bit; (decimalNumber / (2 ** bit) >= 1); bit++) {
             continue;
         };
         highestBit = bit - 1;
-        console.log('highest bit is: ', highestBit);
         const newValue = decimalNumber - 2 ** highestBit;
         if (newValue === 0) {
-            console.log('convertedBinary is: ', convertedBinary);
             if (convertedBinary === null) {
                 return console.log('1' + '0'.repeat(highestBit));
             } else return console.log(`${convertedBinary}1${'0'.repeat(highestBit)}`)
         }
-        console.log('new value is: ', newValue);
-        decimalToBinary(newValue, highestBit);
+        decimalToBinary(newValue, highestBit, '1');
     } else if (bit !== 0) {
         if (convertedBinary === null) {
             convertedBinary = 1;
         }
-        console.log('branching off into recursion.', decimalNumber, bit);
         if (decimalNumber / (2 ** (bit - 1)) >= 1) {
             convertedBinary = `${convertedBinary}1`;
             highestBit = bit - 1;
             const newValue = decimalNumber - 2 ** highestBit;
-            console.log('new value is: ', newValue);
-            console.log('convertedBinary is: ', convertedBinary);
             decimalToBinary(newValue, highestBit, convertedBinary);
         } else {
             convertedBinary = `${convertedBinary}0`;
             highestBit = bit - 1;
-            console.log('convertedBinary is: ', convertedBinary);
-
             decimalToBinary(decimalNumber, highestBit, convertedBinary);
         }
     }
 }
 
-decimalToBinary(5);
+// decimalToBinary(19921227197704231992122719770423);
+decimalToBinary(900719925474088);
