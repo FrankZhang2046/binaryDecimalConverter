74d709f5155181d44dfa3134104d4e26ec83d56e
Frank Zhang <frankbusinessmail@gmail.com>
2020-10-21 22:02:02 -0400

    decimalToBinary convertor function completed

--- script.js
+++ script.js
@@ -1,14 +1,16 @@
 function decimalToBinary (decimalNumber=1, bit = 0, convertedBinary = null) {
     if (decimalNumber === 1 ) {
-        if (convertedBinary === null) {return console.log(decimalNumber)}
-        else return console.log(`${convertedBinary}${'0'.repeat(bit-1)}1`)
+        if (convertedBinary === null) {return ( decimalNumber)}
+        else return (`${convertedBinary}${'0'.repeat(bit-1)}1`)
     };
 
     let highestBit;
 
     if (bit === 0) {
         if (decimalNumber === 0 && convertedBinary !== null) {
-            return console.log(convertedBinary);
+            (decimalNumber, convertedBinary)
+            const returnVal = convertedBinary;
+            return (returnVal);
         }
         for (bit; (decimalNumber / (2 ** bit) >= 1); bit++) {
             continue;
@@ -17,10 +19,10 @@ function decimalToBinary (decimalNumber=1, bit = 0, convertedBinary = null) {
         const newValue = decimalNumber - 2 ** highestBit;
         if (newValue === 0) {
             if (convertedBinary === null) {
-                return console.log('1' + '0'.repeat(highestBit));
-            } else return console.log(`${convertedBinary}1${'0'.repeat(highestBit)}`)
+                return ('1' + '0'.repeat(highestBit));
+            } else return (`${convertedBinary}1${'0'.repeat(highestBit)}`)
         }
-        decimalToBinary(newValue, highestBit, '1');
+        return decimalToBinary(newValue, highestBit, '1');
     } else if (bit !== 0) {
         if (convertedBinary === null) {
             convertedBinary = 1;
@@ -29,25 +31,29 @@ function decimalToBinary (decimalNumber=1, bit = 0, convertedBinary = null) {
             convertedBinary = `${convertedBinary}1`;
             highestBit = bit - 1;
             const newValue = decimalNumber - 2 ** highestBit;
-            decimalToBinary(newValue, highestBit, convertedBinary);
+            return decimalToBinary(newValue, highestBit, convertedBinary);
         } else {
             convertedBinary = `${convertedBinary}0`;
             highestBit = bit - 1;
-            decimalToBinary(decimalNumber, highestBit, convertedBinary);
+            return decimalToBinary(decimalNumber, highestBit, convertedBinary);
         }
     }
 }
 
-let incrementor = 1;
+let incrementer = 1;
 
-setInterval(() => {
-    if (incrementor < 1004) {
-        console.log(`binary of ${incrementor} is: `);
-        decimalToBinary(incrementor);
-        console.log(incrementor.toString(2));
-        incrementor++;
+const functionValidator = setInterval(() => {
+    if (incrementer < (2**53 - 1)) {
+        const myResult = Number(decimalToBinary(incrementer));
+        const officialResult = Number(incrementer.toString(2));
+        console.log(`binary of ${incrementer} is: `);
+        console.log(myResult);
+        console.log(officialResult);
+        if (myResult === officialResult) {
+            incrementer++;
+        } else if (myResult !== officialResult) {
+            console.log(`result is inconsistent for ${incrementer}`);
+            clearInterval(functionValidator);
+        }
     }
 }, 250);
-
-decimalToBinary(35765);
-((35765).toString(2));
